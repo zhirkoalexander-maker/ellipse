e06df132e66bcf18ab4f336ce0c794fdcbaa7c20
Olek Zhyrko <zhirkoalexander-maker@users.noreply.github.com>
2026-07-17 23:41:51 +0200

    VPM 230k + rocket 22

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -5,7 +5,7 @@ export const G = 6.67430e-11;
 export const ORBIT_SCALE = 1e-9;
 
 /** Visual planet size multiplier */
-export const VISUAL_PLANET_MULT = 234000;
+export const VISUAL_PLANET_MULT = 230000;
 
 /** Fixed physics timestep (s) */
 export const FIXED_DT = 1 / 60;
@@ -20,7 +20,7 @@ export const EARTH_MASS = 8.92e25 * 5 * 4 * 1.5 * 6 * 2 * 2 * 2 / 15 * 3;
 export const PART_SCALE = 0.05;
 
 /** Rocket visual scale multiplier */
-export const ROCKET_VISUAL_SCALE = 23;
+export const ROCKET_VISUAL_SCALE = 22;
 
 /** Terrain displacement fraction of visual radius */
 export const TERRAIN_DISP_FRAC = 0.003;
